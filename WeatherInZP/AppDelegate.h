09a600355ea4fa75899c63efe24c70cd600935b8
Victor Krasnohorskiy <hedgefix@gmail.com>
2016-07-06 19:21:43 +0300

    Initial Commit

---
+++ WeatherInZP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherInZP
+//
+//  Created by Виктор on 06.07.16.
+//  Copyright © 2016 Hegefix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherInZP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeatherInZP
+//
+//  Created by Виктор on 06.07.16.
+//  Copyright © 2016 Hegefix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
